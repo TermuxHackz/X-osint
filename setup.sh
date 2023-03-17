d84391087deefe5dd02a17a765745fd55eef02e8
AnonyminHack5 <70668507+TermuxHackz@users.noreply.github.com>
2023-03-17 07:11:53 -0400

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -15,7 +15,7 @@ echo -e "\033[1;91m
    An Open Source Intelligence Framework  
      Created by: AnonyminHack5
      Team: TermuxHackz Society
- Version: 2.0
+ Version: \033[1;92m2.1
    \033[0m"
    echo
 	printf "\e[1;34m\tSelect your terminal to install X-osint \e[0m\n"
@@ -43,6 +43,9 @@ echo
 echo -e "\033[1;91m[*]\033[1;97m Installing All Packages one-time\033[0m"
 pkg install python3
 echo
+echo -e "\033[1;91m[*]\033[1;97m Installing tor \033[0m"
+pkg install tor
+echo
 echo -e "\033[1;91m[*]\033[1;97m Installing Requests\033[0m"
 echo
 pip install requests
@@ -65,20 +68,49 @@ echo
 echo -e "\033[1;91m[*]\033[1;97m Installing google\033[0m" 
 pip install google
 echo
+echo -e "\033[1;91m[*]\033[1;97m Installing tor\033[0m" 
+pkg install tor -y
+echo
 echo -e "\033[1;91m[*]\033[1;97m Installing libxslt\033[0m"
 pkg install libxslt
 echo
-echo -e "\033[1;91m[*]\033[1;97m Installing IMDb\033[0m"
+echo -e "\033[1;91m[*]\033[1;97m Installing IMDb [Might take a while, please be patient]\033[0m"
 pip install imdbpy
 echo
 echo -e "\033[1;91m[*]\033[1;97m Installing prompt-toolkit\033[0m"
 pip install prompt-toolkit
 echo
+echo -e "\033[1;91m[*]\033[1;97m Installing vininfo\033[0m"
+pip install vininfo
+echo
+echo -e "\033[1;91m[*]\033[1;97m Installing tkinter\033[0m"
+pip install tk-tools
+echo
+echo -e "\033[1;91m[*]\033[1;97m Installing phonenumbers\033[0m"
+pip install phonenumbers
+echo
+echo -e "\033[1;91m[*]\033[1;97m Installing update check\033[0m"
+pip install update_check
+echo
+echo -e "\033[1;91m[*]\033[1;97m Installing colorama\033[0m"
+pip install colorama
+echo
+echo -e "\033[1;91m[*]\033[1;97m Installing folium\033[0m"
+pip install folium
+echo
+echo -e "\033[1;91m[*]\033[1;97m Installing opencage\033[0m"
+pip install opencage
+echo
+echo -e "\033[1;91m[*]\033[1;97m Installing Find github\033[0m"
+pip install find-github-email
+echo
+echo -e "\033[1;91m[*]\033[1;97m Installing BeautifulSoup4\033[0m"
+pip install bs4
+echo
 echo -e "\033[1;91m[*]\033[1;97m Setting up environment \033[0m"
 echo
-cp -r xosint.py $PREFIX/bin
+cp -r xosint $PREFIX/bin
 cp -r subdomains.txt $PREFIX/bin
-cd $PREFIX/bin && mv xosint.py xosint
 cd $PREFIX/bin && chmod u+x xosint
 cd $PREFIX/bin && chmod u+x subdomains.txt
 cd $HOME
@@ -96,42 +128,74 @@ sudo apt-get update
 echo
 echo -e "\033[1;91m[*]\033[1;97m Upgrading Linux\033[0m"
 sudo apt-get upgrade -y
+echo
+echo -e "\033[1;91m[*]\033[1;97m Installing tor \033[0m"
+sudo apt install tor -y
 echo 
 echo -e "\033[1;91m[*]\033[1;97m Installing Lolcat\033[0m"
 echo
-sudo pip install lolcat
+pip install lolcat
 echo
 echo -e "\033[1;91m[*]\033[1;97m Installing Pillow\033[0m"
 echo
-sudo pip install pillow==2.2.1
+pip install pillow==2.2.1
 echo
 echo -e "\033[1;91m[*]\033[1;97m Installing resolver\033[0m"
 echo
-sudo pip install resolver
+pip install resolver
 echo
 echo -e "\033[1;91m[*]\033[1;97m Installing piexif\033[0m"
-sudo pip install piexif
+pip install piexif
 sleep 1
 echo
 echo -e "\033[1;91m[*]\033[1;97m Removing old packages\033[0m" 
-sudo python3 -m pip uninstall googlesearch-python -y
-sudo python3 -m pip uninstall google-search -y
-sudo python3 -m pip uninstall google -y
+python3 -m pip uninstall googlesearch-python -y
+python3 -m pip uninstall google-search -y
+python3 -m pip uninstall google -y
 echo
 echo -e "\033[1;91m[*]\033[1;97m Installing google\033[0m" 
-sudo pip install google
+pip install google
+echo
+echo -e "\033[1;91m[*]\033[1;97m Installing tor\033[0m" 
+sudo apt install tor -y
 echo
 echo -e "\033[1;91m[*]\033[1;97m Installing IMDb\033[0m" 
-sudo pip install imdbpy
+pip install imdbpy
 echo
 echo -e "\033[1;91m[*]\033[1;97m Installing prompt-toolkit\033[0m"
-sudo pip install prompt-toolkit
+pip install prompt-toolkit
+echo
+echo -e "\033[1;91m[*]\033[1;97m Installing phonenumbers\033[0m"
+pip install phonenumbers
+echo
+echo -e "\033[1;91m[*]\033[1;97m Installing colorama\033[0m"
+pip install colorama
+echo
+echo -e "\033[1;91m[*]\033[1;97m Installing BeautifulSoup4\033[0m"
+pip install bs4
+echo
+echo -e "\033[1;91m[*]\033[1;97m Installing update check\033[0m"
+pip install update_check
+echo
+echo -e "\033[1;91m[*]\033[1;97m Installing Find github\033[0m"
+pip install find-github-email
+echo
+echo -e "\033[1;91m[*]\033[1;97m Installing folium\033[0m"
+pip install folium
+echo
+echo -e "\033[1;91m[*]\033[1;97m Installing opencage\033[0m"
+pip install opencage
+echo
+echo -e "\033[1;91m[*]\033[1;97m Installing vininfo\033[0m"
+pip install vininfo
+echo
+echo -e "\033[1;91m[*]\033[1;97m Installing tkinter\033[0m"
+pip install tk-tools
 echo
 printf "\e[1;94mSetting up environment for Linux .. \e[0m\n"				
 sleep 1
 echo
-sudo cp -r xosint.py /usr/local/bin
-cd /usr/local/bin && sudo mv xosint.py xosint
+sudo cp -r xosint /usr/local/bin
 cd /usr/local/bin && sudo chmod u+x xosint
 cd $HOME && sudo rm -rf X-osint
  printf "\e[1;91m[*]\e[1;97m X-osint Installed Successfully, Now its ready for use. So re-open your Linux Terminal To use X-osint\e[0m\n"
